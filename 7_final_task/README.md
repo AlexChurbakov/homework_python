9b96ed012ef2a54b75028e337f3a6f41e7aa0c0c
Alex <->
2024-12-28 22:42:46 +0300

    bloc 7

---
+++ 7_final_task/README.md
@@ -0,0 +1,8 @@
+Запуск программы:
+
+1. Установить python 3.11+
+2. В директории с файлами приложения:
+   2.1. python -m venv %venvname%
+   2.2. %venvname%\Script\activate.bat для Windows или source %venvname%/bin/activate для Linux/Mac
+3. pip install -r requirements.txt
+4. python main.py
--- 7_final_task/main.py
+++ 7_final_task/main.py
@@ -6,32 +6,38 @@ from pre_commit.output import write_line
 import requests
 import geocoder
 import json
-from functions import *
-from consts import *
 
-
-"""
-Текущее время: 2023-10-03 09:48:47+03:00
-Название города: Санкт-Петербург
-Погодные условия: облачно
-Текущая температура: 12 градусов по цельсию
-Ощущается как: 11 градусов по цельсию
-Скорость ветра: 5 м/c
-"""
-
-def __main__ ():
+def main ():
     program_activity = True
     while program_activity:
-        print ("Введите название города, погоду в котором хотите узнать\nИли введите 0, если хотите завершить программу\n")
-        sity: str
-        sity = input()
-        if sity == "0":
+        print ("""Введите: 0, если хотите завершить программу\n"
+                           1, чтоб узнать погоду в выбранном городе\n
+                           2, чтоб узнать погоду в Вашем городе\n
+                           3, чтоб посмотреть историю запросов\n
+                           4, чтоб стереть историю запросов\n""")
+        swithc_key = input()
+        if swithc_key == "0":
             program_activity = False
+            continue
+        elif swithc_key == "1":
+            get_weather_selected_sity()
+        elif swithc_key == "2":
+            get_weather_your_sity()
+        elif swithc_key == "3":
+            read_story_file()
+        elif swithc_key == "4":
+            clear_story_file()
+        else:
+            print ("Вы указали неверный вариант. Попробуйте ещё раз\n")
             continue
 
 
+global_weather = {"time": "", "city": "", "weather_conditions": "", "temperature": "", "temperature_feels": "", "wind_speed": ""}
 
 
+def get_URL(city: str):
+    API_KEY = open("api_key.json", "r").read()
+    return "http://api.openweathermap.org/data/2.5/weather?appid=" + API_KEY + "&q=" + city + "&lang=ru" + "&units=metric"
 
 
 def print_weather_of_city (weather: dict):
@@ -43,125 +49,92 @@ def print_weather_of_city (weather: dict):
     print (f"Скорость ветра: {weather["wind_speed"]} м/c\n")
 
 
-
-
-
-
-
-
-
-
-
-
-BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
-API_KEY = open("api_key.txt", "r").read()
-
-
-def generate_url(city: str):
-    return BASE_URL + "appid=" + API_KEY + "&q=" + city + "&lang=ru" + "&units=metric"
-
-
-def get_user_city():
-    return geocoder.ip("me").city
-
-
-def save_weather_data(weather_data):
-    with open('wd.json', 'a') as file:
-        json.dump(weather_data.data, file, indent=0)
-
-
-def parse_weather_data(resp):
-    try:
-        timezone = datetime.timezone(datetime.timedelta(seconds=float(resp["timezone"])))
-        time = datetime.datetime.fromtimestamp(float(resp["dt"]), timezone)
-    except KeyError:
-        time = resp["time"]
-        # для чтения из json
-    name = resp["name"]
-    description = resp["weather"][0]["description"]
-    temp_celsius = resp["main"]["temp"]
-    feels_like_celsius = resp["main"]["feels_like"]
-    speed = resp["wind"]["speed"]
-    weatherdata = WeatherData(time, name, description, temp_celsius, feels_like_celsius, speed)
-    return weatherdata
-
-
-def weather_for_selected_city():
-    city = input("Введите город:\n").replace(" ", "")
-    url = generate_url(city)
+def get_weather_selected_sity():
+    city = input("Введите название города:\n").replace(" ", "")
+    url = get_URL(city)
     response = requests.get(url).json()
     if response["cod"] == 200:
-        weatherdata = parse_weather_data(response)
-        save_weather_data(weatherdata)
-        print(weatherdata, "\n")
+        weather_info = write_weather_info(response)
+        save_info_to_file(make_string(weather_info))
+        print_weather_of_city(weather_info)
     else:
         print(f"Ошибка {response['cod']}: {response['message']}")
 
 
-def weather_for_user_city():
-    city = get_user_city()
-    url = generate_url(city)
+def get_weather_your_sity():
+    city = geocoder.ip("me").city
+    url = get_URL(city)
     response = requests.get(url).json()
-    weatherdata = parse_weather_data(response)
-    save_weather_data(weatherdata)
-    print(weatherdata, "\n")
+    weather_info = write_weather_info(response)
+    save_info_to_file(weather_info)
+    print_weather_of_city(weather_info)
 
 
-
-
-    def read_history():
-        try:
-            n = int(input("Сколько запросов?\n"))
-            weather_datas = read_weather_data(n)
-            weather_datas.reverse()
-            for wd in weather_datas:
-                wdprint = WeatherData(*wd.values())
-                print(f"{wdprint}\n")
-                del wdprint
-        except ValueError:
-            print('Число запросов выражается целым положительным числом')
-
-    def read_weather_data(n: int):
+def read_story_file():
+    try:
+        n = int(input("Сколько последних запросов Вы хотите посмотреть?\n"))
         if (type(n) != int) or (n < 1):
             raise ValueError
         else:
-            data_array = []
-            with open('wd.json', 'r') as file:
-                remainder = file.read().replace("\n", "")
-                while len(remainder) > 2:
-                    data_array.append(remainder.partition("}")[0] + "}")
-                    remainder = remainder.partition("}")[2]
-            weather_data_array = []
-            i = 1
-            data_array.reverse()
-            for d in data_array:
-                if i > n:
-                    break
-                js = json.loads(d)
-                weather_data_array.append(js)
-                i = i + 1
-            return weather_data_array
+            with open('weather_data_file.txt', 'r') as file:
+                new_n = n
+                while new_n >= 1:
+                    for index in range(7):
+                        file_info = file.readline()
+                        if file_info == "":
+                            print("Больше в файле нет информации\n")
+                            break
+                        print(file_info, "\n")
+                    new_n -= 1
+    except ValueError:
+        print("Число запросов выражается целым положительным числом\n")
 
-    def clear_history():
-        with open("wd.json", "w") as file:
-            pass
 
-    options = {"1": weather_for_user_city,
-               "2": weather_for_selected_city,
-               "3": read_history,
-               "4": clear_history,
-               "5": ""}
+def clear_story_file():
+    with open("weather_data_file.txt", "w") as file:
+        pass
 
-    def enable_cycle():
-        while True:
-            answer = input(answer_prompt).replace(" ", "")
-            if answer in options.keys():
-                if answer == "5":
-                    break
-                else:
-                    options[answer]()
-            else:
-                print('Нет такой опции')
 
-    if __name__ == "__main__":
-        enable_cycle()
+def save_info_to_file(file_info):
+    with open('weather_data_file.txt', 'a') as file:
+        file.write(file_info)
+
+
+def make_string(weather) -> str:
+    file_info = f"\
+    Текущее время: {weather["time"]}\n\
+    Название города: {weather["city"]}\n\
+    Погодные условия: {weather["weather_conditions"]}\n\
+    Текущая температура: {weather["temperature"]} градусов по цельсию\n\
+    Ощущается как: {weather["temperature_feels"]} градусов по цельсию\n\
+    Скорость ветра: {weather["wind_speed"]} м/c\n"
+    return file_info
+
+def write_weather_info(resp):
+    try:
+        timezone = datetime.timezone(datetime.timedelta(seconds=float(resp["timezone"])))
+        time = datetime.datetime.fromtimestamp(float(resp["dt"]), timezone)
+    except KeyError:
+        print("Ошибка\n")
+
+    weather = global_weather
+    weather["time"] = resp["time"]
+    weather["city"] = resp["name"]
+    weather["weather_conditions"] = resp["weather"][0]["description"]
+    weather["temperature"] = resp["main"]["temp"]
+    weather["temperature_feels"] = resp["main"]["feels_like"]
+    weather["wind_speed"] = resp["wind"]["speed"]
+    return weather
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+# сейчас файл читает всегда сначала, нужно сделать, чтоб читал с конца
+
+
+
+
+
